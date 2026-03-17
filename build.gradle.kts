d694bcdd03490d6d46ffef10df2df25dc4d84e1c
Mallne <info@mallne.cloud>
2026-03-17 23:17:47 +0100

    Align Dependencies and better support for JS Target

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ plugins {
     alias(libs.plugins.kjvm)
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ktor)
-    alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.version.catalog.update)
     alias(libs.plugins.ben.manes.versions)
     alias(libs.plugins.dependency.analysis)
+    alias(libs.plugins.koin.compiler)
 }
 
 allprojects {
@@ -24,7 +24,6 @@ application {
 
 dependencies {
     implementation(libs.dc.areaassist.shared)
-    ksp(libs.koin.ksp)
     implementation(libs.dc.synapse.core)
     runtimeOnly(libs.postgres)
 }
